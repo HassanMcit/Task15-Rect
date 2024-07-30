a51461f5589534644d3cfcde45b633b711baba7b
HassanMcit <hassan.a173784@gmail.com>
2024-07-30 22:25:52 +0300

    'nine'

--- src/Component/Notfound/Notfound.jsx
+++ src/Component/Notfound/Notfound.jsx
@@ -4,7 +4,7 @@ export default function Notfound() {
     return (
         <>
             <div className='h-screen flex justify-center items-center bg-[#1ABC9C]'>
-                <div className='text-3xl md:text-5xl font-bold text-white text-center'>
+                <div className='text-3xl md:text-5xl font-bold text-white text-center mt-10'>
                     Page Not Found
                     <p className='my-4'>This page doesn’t exist.</p>
                 </div>
