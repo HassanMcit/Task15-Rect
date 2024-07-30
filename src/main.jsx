3fdace0a1d1bfa7ddb0b09fa36f64bc3cd94bd7d
HassanMcit <hassan.a173784@gmail.com>
2024-07-30 22:06:05 +0300

    'six'

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>,
+  </React.StrictMode>,
 )
